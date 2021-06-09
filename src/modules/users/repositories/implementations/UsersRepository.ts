5b4667a553d358399f5158037f2dfe471795ca53
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-08 21:09:04 -0300

    Update UsersRepository.ts

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -16,26 +16,13 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    const user = await this.repository.findOne(user_id);
-    const users_games_games: Users_games_gamesReturn = await this.repository.query(`SELECT * FROM users_games_games WHERE "usersId" = '${user_id}'`);
-    const finalArrayGamesUser: Game[] = [];
-
-    await Promise.all(users_games_games.map(async (obj): Promise<void> => {
-      const [game]: Game[] = await this.repository.query(`SELECT * FROM games WHERE id = '${obj.gamesId}'`);
-      finalArrayGamesUser.push(game);
-      return;
-    }));
-
-    const result = {
-      ...user,
-      games: finalArrayGamesUser,
-    }
-
-    return result as User;
-  }
+async findUserWithGamesById({
+  user_id,
+}: IFindUserWithGamesDTO): Promise<User> {
+  return this.repository.findOneOrFail(user_id, {
+    relations: ['games'],
+  });
+}
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
     return this.repository.query('SELECT * FROM users order by first_name');
